2ddc6e986b4bd5b2b11a2aa12a25313a7cbf384d
Eric Price <eric@ericprice.cc>
2013-05-07 10:05:43 -0500

    some reversions fixed

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
             lastSlideId = slide.id; // record this so we know what we're leaving next time
         }
     });
-    
     L.DomUtil.disableTextSelection();
     Zoomer.zoom_image_by_class({"container":"image-view-2", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_786/{z}_{x}_{y}.jpg", "imageWidth": 4000, "imageHeight": 3187});
     Zoomer.zoom_image_by_class({"container":"image-view-1", "tileURL": "http://cdn{s}.walkerart.org/wac_786/{z}_{x}_{y}.jpg", "imageWidth": 4000, "imageHeight": 3187});
@@ -28,29 +27,21 @@ $(document).ready(function() {
     Zoomer.zoom_image_by_class({"container":"image-view-3", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_3691/{z}_{x}_{y}.jpg", "imageWidth": 1445, "imageHeight": 1920});
     Zoomer.zoom_image_by_class({"container":"image-view-4", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_11156/{z}_{x}_{y}.jpg", "imageWidth": 5920, "imageHeight": 3893});
     
-    _V_('video1_player').ready(function(){
-        // Store the video object
-        var myPlayer = this; 
-        // Make up an aspect ratio
-        var aspectRatio = 264/640; 
-        function resizeVideoJS(){
-            var width = document.getElementById(myPlayer.id).parentElement.offsetWidth;
-            var w=window.innerWidth;
-            if (width > w){
-                myPlayer.width(w).height( w * aspectRatio );
-            } else {
-                myPlayer.width(width).height( width * aspectRatio );
-            }
-        }
-        // Initialize resizeVideoJS()
-        resizeVideoJS();
-        // Then on resize call resizeVideoJS()
-        var id;
-        $(window).resize(function() {
-            clearTimeout(id);
-            id = setTimeout(resizeVideoJS, 100);
-        });
-    });
+    MediaElement('player1', {success: function(me) {
+        me.play();
+
+        me.addEventListener('timeupdate', function() {
+            document.getElementById('time').innerHTML = me.currentTime;
+        }, false);
+
+        document.getElementById('pp')['onclick'] = function() {
+            if (me.paused)
+                me.play();
+            else
+                me.pause();
+        };
+
+    }});
     
     $('.tombstone').click(function() {
         artworkInfo = $(this).parent().children('.info').html();
