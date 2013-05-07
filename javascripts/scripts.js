348d1ebf2cab1beb74255e28af318ea5e45fffb0
Eric Price <eric@ericprice.cc>
2013-05-07 10:29:21 -0500

    working lightbox

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -27,25 +27,35 @@ $(document).ready(function() {
     Zoomer.zoom_image_by_class({"container":"image-view-3", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_3691/{z}_{x}_{y}.jpg", "imageWidth": 1445, "imageHeight": 1920});
     Zoomer.zoom_image_by_class({"container":"image-view-4", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_11156/{z}_{x}_{y}.jpg", "imageWidth": 5920, "imageHeight": 3893});
     
-    MediaElement('player1', {success: function(me) {
-        me.play();
-
-        me.addEventListener('timeupdate', function() {
-            document.getElementById('time').innerHTML = me.currentTime;
-        }, false);
-
-        document.getElementById('pp')['onclick'] = function() {
-            if (me.paused)
-                me.play();
-            else
-                me.pause();
-        };
-
-    }});
+    //MediaElement('player1', {success: function(me) {
+    //    me.play();
+    //
+    //    me.addEventListener('timeupdate', function() {
+    //        document.getElementById('time').innerHTML = me.currentTime;
+    //    }, false);
+    //
+    //    document.getElementById('pp')['onclick'] = function() {
+    //        if (me.paused)
+    //            me.play();
+    //        else
+    //            me.pause();
+    //    };
+    //
+    //}});
     
     $('.tombstone').click(function() {
-        artworkInfo = $(this).parent().children('.info').html();
+        //artworkInfo = $(this).parent().children('.info').html();
+        artworkInfo = '<article class="info">\
+                           <p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et.</p>\
+                           <p>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris.</p>\
+                       </article>';
         $.colorbox({
+            transition: 'fade',
+            width: '50%',
+            height: '90%',
+            speed: 150,
+            fadeOut: 250,
+            opacity: 0.6,
             html: artworkInfo
         });
     });
