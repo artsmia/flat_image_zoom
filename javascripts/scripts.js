257c84f1e46387ef5e12240f7bfb264c41df973b
AJ Warnick <anthony.warnick@walkerart.org>
2013-05-08 12:11:24 -0500

    add poster

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -3,90 +3,94 @@ var lastSlideId = 'image-view-1',
     tombstoneTimeout = '',
     tombstoneDuration = 3000,
     introTimeout = '',
-    introDuration = 60000;
-    
+    slides = {},
+    introDuration = 60000,
+    zoomer_template = _.template($('#zoomer').html());
+
 function displayTombstone() {
-    clearTimeout(tombstoneTimeout);
-    $('.tombstone').stop(true, true).delay(250).fadeIn(150);
-    tombstoneTimeout = setTimeout(function() {
-        $('.tombstone').stop(true, true).fadeOut(250);
-    }, tombstoneDuration);
+   clearTimeout(tombstoneTimeout);
+   $('.tombstone').stop(true, true).delay(250).fadeIn(150);
+   tombstoneTimeout = setTimeout(function() {
+       $('.tombstone').stop(true, true).fadeOut(250);
+   }, tombstoneDuration);
 }
 
 function hideIntro() {
-    clearTimeout(introTimeout);
-    $('.intro-wrapper').hide();
-    introTimeout = setTimeout(function() {
-        $('.intro-wrapper').show();
-    }, introDuration);
+   clearTimeout(introTimeout);
+   $('.intro-wrapper').hide();
+   introTimeout = setTimeout(function() {
+       $('.intro-wrapper').show();
+   }, introDuration);
 }
+        
+function slideInit(){
+ window.mySwipe = new Swipe(document.getElementById('slider'), {
+     callback: function(index,slide) {
+         //console.log(slide.id)
+         if (Zoomer.zoomers[lastSlideId]) {
+             Zoomer.zoomers[lastSlideId].map.centerImageAtExtents();
+         }
+         var videoId = lastSlideId+"_player";
+         if ($('#'+videoId).length) {
+             var myPlayer = _V_(videoId);
+             if (myPlayer) {
+                 myPlayer.pause();
+                 //myPlayer.currentTime(0); // removes image? Maybe poster would help?
+             }
+         }
+         lastSlideId = slide.id; // record this so we know what we're leaving next time
+     }
+ });
 
-$(document).ready(function() {
+}   
     
+    
+$.getJSON('javascripts/test.json', function(data) {
+  slides = data.slides;
+
+  for (variable in slides) {
+    var clss = "test_class" + variable;
+    slides[variable].zoomer_class = clss;
+    $(".swipe-wrap").append(zoomer_template(slides[variable]));
+    if(slides[variable].type == "zoomer"){
+      Zoomer.zoom_image_by_class({"container":slides[variable].zoomer_class, "tileURL": slides[variable].zoomer_url, "imageWidth": slides[variable].zoomer_width, "imageHeight": slides[variable].zoomer_height});
+    }else if(slides[variable].type == "video"){
+      MediaElement(slides[variable].player_id);
+    }
+    
+  };
+  slideInit();
+});
+
+       
+
+
+
+
+$(document).ready(function() {   
     displayTombstone();
-    
-    window.mySwipe = new Swipe(document.getElementById('slider'), {
-        callback: function(index,slide) {
-            displayTombstone();
-            //console.log(slide.id)
-            if (Zoomer.zoomers[lastSlideId]) {
-                Zoomer.zoomers[lastSlideId].map.centerImageAtExtents();
-            }
-            var videoId = lastSlideId+"_player";
-            if ($('#'+videoId).length) {
-                var myPlayer = _V_(videoId);
-                if (myPlayer) {
-                    myPlayer.pause();
-                    //myPlayer.currentTime(0); // removes image? Maybe poster would help?
-                }
-            }
-            lastSlideId = slide.id; // record this so we know what we're leaving next time
-        }
-    });
     L.DomUtil.disableTextSelection();
-    Zoomer.zoom_image_by_class({"container":"image-view-2", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_786/{z}_{x}_{y}.jpg", "imageWidth": 4000, "imageHeight": 3187});
-    Zoomer.zoom_image_by_class({"container":"image-view-1", "tileURL": "http://cdn{s}.walkerart.org/wac_786/{z}_{x}_{y}.jpg", "imageWidth": 4000, "imageHeight": 3187});
-    //Zoomer.zoom_image_by_class({"container":"image-view-1", "tileURL": 'https://stewart.walkerart.org/zoomtest/wac_1729/{z}_{x}_{y}.jpg', "imageWidth": 2560, "imageHeight": 1702});
-    Zoomer.zoom_image_by_class({"container":"image-view-3", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_3691/{z}_{x}_{y}.jpg", "imageWidth": 1445, "imageHeight": 1920});
-    Zoomer.zoom_image_by_class({"container":"image-view-4", "tileURL": "https://stewart.walkerart.org/zoomtest/wac_11156/{z}_{x}_{y}.jpg", "imageWidth": 5920, "imageHeight": 3893});
-    
-    //MediaElement('player1', {success: function(me) {
-    //    me.play();
-    //
-    //    me.addEventListener('timeupdate', function() {
-    //        document.getElementById('time').innerHTML = me.currentTime;
-    //    }, false);
-    //
-    //    document.getElementById('pp')['onclick'] = function() {
-    //        if (me.paused)
-    //            me.play();
-    //        else
-    //            me.pause();
-    //    };
-    //
-    //}});
-    
-    $(document).click(function() {
-        displayTombstone();
-        hideIntro();
-    });
-    
-    $('.info-link').click(function(event) {
-        event.stopPropagation();
-        event.preventDefault();
-        //artworkInfo = $(this).parent().children('.info').html();
-        artworkInfo = '<article class="info">\
-                           <p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. <em>Praesent id metus massa, ut blandit odio.</em> Proin quis tortor orci. Etiam at risus et lorem ipsum.</p>\
-                           <p>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>\
-                       </article>';
-        $.colorbox({
-            transition: 'none',
-            width: '60%',
-            initialWidth: '50%',
-            fadeOut: 250,
-            opacity: 0.8,
-            html: artworkInfo
-        });
-    });
+});
 
+$(document).on("click", function(event){
+  displayTombstone();
+  hideIntro();
+});
+
+$('.info-link').on("click", function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    //artworkInfo = $(this).parent().children('.info').html();
+    artworkInfo = '<article class="info">\
+                       <p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. <em>Praesent id metus massa, ut blandit odio.</em> Proin quis tortor orci. Etiam at risus et lorem ipsum.</p>\
+                       <p>Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>\
+                   </article>';
+    $.colorbox({
+        transition: 'none',
+        width: '60%',
+        initialWidth: '50%',
+        fadeOut: 250,
+        opacity: 0.8,
+        html: artworkInfo
+    });
 });
